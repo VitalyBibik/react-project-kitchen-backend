615596faf28ec303ce239e60b2805881cdf9d754
Sergey Rogalskiy <rogalskiy.sergey@gmail.com>
2021-05-26 19:05:51 +0300

    added people to tags :)

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -1,11 +1,18 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
 var Article = mongoose.model('Article');
+var User = mongoose.model('User');
 
 // return a list of tags
 router.get('/', function(req, res, next) {
   Article.find().distinct('tagList').then(function(tags){
     return res.json({tags: tags});
+  }).catch(next);
+});
+
+router.get('/people', function(req, res, next) {
+  User.find().then(function(people){
+    return res.json({people: people});
   }).catch(next);
 });
 
